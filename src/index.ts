65f1d12a4bf1d96b57af449ced45648b0440507c
oussama <khelefoussama2005@gmail.com>
2025-10-18 22:10:55 +0100

    mount main router to the main application

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Hono } from 'hono'
+import mainRouter from './routes'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route('/api',mainRouter);
 
 export default app
