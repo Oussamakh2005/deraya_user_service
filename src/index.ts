257bc983e373d76e3ca8024f4f506e09640acdb1
oussama <khelefoussama2005@gmail.com>
2025-10-25 08:41:41 +0100

    config cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono'
 import mainRouter from './routes'
+import {cors} from "hono/cors";
 
 const app = new Hono()
-
+app.use('/api/*',cors({
+    origin : "*"
+}));
 app.route('/api',mainRouter);
 
 export default app
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,6 +4,7 @@ import addDoctor from "../controllers/user/addDoctor";
 import login from "../controllers/auth/login";
 import isAdmin from "../middlewares/isAdmin";
 import authenticated from "../middlewares/authenticated";
+import getAllDoctors from "../controllers/user/getAllDoctors";
 
 const userRouter = new Hono();
 
@@ -11,6 +12,8 @@ const userRouter = new Hono();
 userRouter.post('/user',addUser);
 //register doctor :
 userRouter.post('/doctor',authenticated,isAdmin,addDoctor);
+//get all doctors :
+userRouter.get('/doctor',authenticated,isAdmin,getAllDoctors);
 //login :
 userRouter.post('/login',login);
 //check if user authenticated :
